b2582096e80e6271634253166e31294238411680
carmelocatalfamo <carmelo.catalfamo92@gmail.com>
2020-07-05 08:45:40 +0200

    Update seo tag

--- src/server/metadata.js
+++ src/server/metadata.js
@@ -3,88 +3,88 @@ const find = require('lodash/find')
 const metadataList = [
   {
     path: '/',
-    title: 'Statistiche COVID-19 Italia',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Italia.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Italia',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Italia prima e dopo la fase 2.'
   }, {
     path: '/piemonte',
-    title: 'Statistiche COVID-19 Piemonte',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Piemonte.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Piemonte',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Piemonte prima e dopo la fase 2.'
   }, {
     path: '/valledaosta',
-    title: "Statistiche COVID-19 Valle d'Aosta",
-    description: "Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Valle d'Aosta."
+    title: "Coronavirus: numeri e grafici sul COVID-19 in Valle d'Aosta",
+    description: "Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Valle d'Aosta prima e dopo la fase 2."
   }, {
     path: '/lombardia',
-    title: 'Statistiche COVID-19 Lombardia',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Lombardia.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Lombardia',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Lombardia prima e dopo la fase 2.'
   }, {
     path: '/trentinoaltoadige',
-    title: 'Statistiche COVID-19 Trentino-Alto adige',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Trentino-Alto adige.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Trentino-Alto adige',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Trentino-Alto adige prima e dopo la fase 2.'
   }, {
     path: '/veneto',
-    title: 'Statistiche COVID-19 Veneto',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Veneto.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Veneto',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Veneto prima e dopo la fase 2.'
   }, {
     path: '/friuliveneziagiulia',
-    title: 'Statistiche COVID-19 Friuli-Venezia giulia',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Friuli-Venezia giulia.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Friuli-Venezia Giulia',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Friuli-Venezia Giulia prima e dopo la fase 2.'
   }, {
     path: '/liguria',
-    title: 'Statistiche COVID-19 Liguria',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Liguria.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Liguria',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Liguria prima e dopo la fase 2.'
   }, {
     path: '/emiliaromagna',
-    title: 'Statistiche COVID-19 Emilia-Romagna',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Emilia-Romagna.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Emilia-Romagna',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Emilia-Romagna prima e dopo la fase 2.'
   }, {
     path: '/toscana',
-    title: 'Statistiche COVID-19 Toscana',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Toscana.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Toscana',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Toscana prima e dopo la fase 2.'
   }, {
     path: '/umbria',
-    title: 'Statistiche COVID-19 Umbria',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Umbria.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Umbria',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Umbria prima e dopo la fase 2.'
   }, {
     path: '/marche',
-    title: 'Statistiche COVID-19 Marche',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Marche.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Marche',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Marche prima e dopo la fase 2.'
   }, {
     path: '/lazio',
-    title: 'Statistiche COVID-19 Lazio',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Lazio.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Lazio',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Lazio prima e dopo la fase 2.'
   }, {
     path: '/abruzzo',
-    title: 'Statistiche COVID-19 Abruzzo',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Abruzzo.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Abruzzo',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Abruzzo prima e dopo la fase 2.'
   }, {
     path: '/molise',
-    title: 'Statistiche COVID-19 Molise',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Molise.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Molise',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Molise prima e dopo la fase 2.'
   }, {
     path: '/campania',
-    title: 'Statistiche COVID-19 Campania',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Campania.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Campania',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Campania prima e dopo la fase 2.'
   }, {
     path: '/puglia',
-    title: 'Statistiche COVID-19 Puglia',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Puglia.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Puglia',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Puglia prima e dopo la fase 2.'
   }, {
     path: '/basilicata',
-    title: 'Statistiche COVID-19 Basilicata',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Basilicata.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Basilicata',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Basilicata prima e dopo la fase 2.'
   }, {
     path: '/calabria',
-    title: 'Statistiche COVID-19 Calabria',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Calabria.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Calabria',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Calabria prima e dopo la fase 2.'
   }, {
     path: '/sicilia',
-    title: 'Statistiche COVID-19 Sicilia',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Sicilia.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Sicilia',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Sicilia prima e dopo la fase 2.'
   }, {
     path: '/sardegna',
-    title: 'Statistiche COVID-19 Sardegna',
-    description: 'Rappresentazione grafica dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Sardegna.'
+    title: 'Coronavirus: numeri e grafici sul COVID-19 in Sardegna',
+    description: 'Numeri, grafici e statistiche dei dati ufficiali forniti dalla Protezione Civile sul COVID-19 in Sardegna prima e dopo la fase 2.'
   }
 ]
 

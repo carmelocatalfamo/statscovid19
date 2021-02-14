e0b2ce2b09c483bb3c8c3a55705742ae0d26e769
carmelocatalfamo <carmelo.catalfamo92@gmail.com>
2021-02-14 01:21:16 +0100

    Fix endpoint region

--- pages/api/zones.tsx
+++ pages/api/zones.tsx
@@ -1,8 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import path from 'path'
+import fs from 'fs'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const json = await require('../../public/zones.json')
+    const filePath = path.resolve('public/zones.json')
+    const content = fs.readFileSync(filePath, 'utf-8')
+    const json = JSON.parse(content)
     res.status(200).json(json)
   } catch (error) {
     res.status(400).json({ success: false })

e1c81285ceb8610d267bdd6c316ba4d8f7a7fd7e
carmelocatalfamo <carmelo.catalfamo92@gmail.com>
2021-02-20 12:26:51 +0100

    Remove docker

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-cd statscovid19
-git pull origin production
-cd ..
-docker-compose stop statscovid19
-docker-compose rm -f statscovid19
-docker-compose build statscovid19
-docker-compose up -d statscovid19
